07734c44ec4978996c34b5a240392bf5f1eadb55
Yasmin Lopes <yclopes27@gmail.com>
2023-11-25 16:04:08 -0300

    first commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native';
+import Header from '../../components/Header';
+
+export default function Home() {
+  return (
+    <View>
+      <Header
+        name="Yasmin Lopes"/>
+      <Text>Home works!</Text>
+    </View>
+  );
+}
